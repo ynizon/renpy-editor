b2f754f7824ea625982a86f34a94d7cfafca7537
Yohann Nizon <ynizon@gmail.com>
2019-09-12 17:16:14 +0200

    FIX

--- resources/views/action/edit.blade.php
+++ resources/views/action/edit.blade.php
@@ -5,7 +5,7 @@ $key = $params["element"]."_".$params["subject_id"]."_".$params["verb"];
 switch ($params["verb"]){
 	case "move":
 		?>
-		$("#move").val(("<?php echo ($params["info"]);?>"));
+		$("#move").val(decodeURIComponent("<?php echo rawurlencode($params["info"]);?>"));
 		<?php
 		break;
 		
@@ -16,21 +16,21 @@ switch ($params["verb"]){
 		
 	case "say":
 		?>
-		$("#say").val(("<?php echo ($params["info"]);?>"));
+		$("#say").val(decodeURIComponent("<?php echo rawurlencode($params["info"]);?>"));
 		<?php
 		break;
 		
 	case "menu":
 		$infos = json_decode($params["info"],true);
 		?>
-		$("#menu1").val("<?php echo ($infos["menu1"]);?>");
-		$("#menu2").val("<?php echo ($infos["menu2"]);?>");
-		$("#menu3").val("<?php echo ($infos["menu3"]);?>");
-		$("#menu4").val("<?php echo ($infos["menu4"]);?>");
-		$("#menu1_to").val("<?php echo ($infos["menu1_to"]);?>");
-		$("#menu2_to").val("<?php echo ($infos["menu2_to"]);?>");
-		$("#menu3_to").val("<?php echo ($infos["menu3_to"]);?>");
-		$("#menu4_to").val("<?php echo ($infos["menu4_to"]);?>");
+		$("#menu1").val(decodeURIComponent("<?php echo rawurlencode($infos["menu1"]);?>");
+		$("#menu2").val(decodeURIComponent("<?php echo rawurlencode($infos["menu2"]);?>");
+		$("#menu3").val(decodeURIComponent("<?php echo rawurlencode($infos["menu3"]);?>");
+		$("#menu4").val(decodeURIComponent("<?php echo rawurlencode($infos["menu4"]);?>");
+		$("#menu1_to").val(decodeURIComponent("<?php echo rawurlencode($infos["menu1_to"]);?>");
+		$("#menu2_to").val(decodeURIComponent("<?php echo rawurlencode($infos["menu2_to"]);?>");
+		$("#menu3_to").val(decodeURIComponent("<?php echo rawurlencode($infos["menu3_to"]);?>");
+		$("#menu4_to").val(decodeURIComponent("<?php echo rawurlencode($infos["menu4_to"]);?>");
 		
 		<?php
 		break;
