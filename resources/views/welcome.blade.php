295f36da10c59ee6018c95076cd61c2106d2fd6f
Yohann Nizon <ynizon@gmail.com>
2019-09-20 12:03:43 +0200

    VIdeo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -105,7 +105,11 @@
 					<a href="https://www.renpy.org/doc/html/">Ren'py Doc</a>
 					<br/>					<br/>					
                 </div>
-				<br/>					<br/>					<br/>					<br/>					
+				<br/>					<br/>
+                    
+                    <script src="https://fast.wistia.com/embed/medias/5g9578thfk.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_5g9578thfk videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/5g9578thfk/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+                    
+                    <br/>					<br/>					
 				<footer >
 					<div class="container">
 						<div class="row">
